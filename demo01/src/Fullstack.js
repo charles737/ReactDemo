02dfa30cad627e1bf22db980df0be8b4134fd690
charles737 <cl426886@gmail.com>
2020-11-05 14:33:22 +0800

    update-生命周期函数

--- demo01/src/Fullstack.js
+++ demo01/src/Fullstack.js
@@ -1,5 +1,6 @@
 import React,{Component, Fragment} from 'react'
 import './style.css'
+import FullstackItem from './FullstackItem'
 
 class Fullstack extends Component{
 
@@ -11,35 +12,89 @@ class Fullstack extends Component{
         }
     }
 
+    /**
+     * 生命周期函数，对代码主体没有影响
+     */
+    componentWillMount(){
+        console.log('componentWillMount---组件将要挂载到页面的时刻')
+    }
+
+    /**
+     * 生命周期函数
+     */
+    componentDidMount(){
+        console.log('componentDidMount---组件挂载完成时刻')
+    }
+
+    /**
+     * 生命周期函数
+     * 在render之前执行，必须返回布尔值，true执行render，false不执行
+     * 作用：优化组件性能
+     */
+    
+    shouldComponentUpdate(){
+        console.log('1-shouldComponentUpdate')
+        return true
+    }
+
+    /**
+     * 生命周期函数
+     */
+    componentWillUpdate(){
+        console.log('2-componentWillUpdate')
+    }
+
+    /**
+     * 生命周期函数
+     */
+    componentDidUpdate(){
+        console.log('4-componentDidUpdate')
+    }
+
+    /**
+     * render - 渲染
+     */
     render(){
+        // 生命周期函数
+        console.log('3-render---组件挂载中')
         return (
-            // 1. 第一种写法
-            // <div>
-            //     <div><input /><button>增加需求</button></div>
-            //     <ul>
-            //         <li>UI设计</li>
-            //         <li>前端设计</li>
-            //     </ul>
-            // </div>
-            // 2. 第二种写法，少一层div包裹，可以省略一些css
+            /**
+             * 1. 第一种写法
+            <div>
+                <div><input /><button>增加需求</button></div>
+                <ul>
+                    <li>UI设计</li>
+                    <li>前端设计</li>
+                </ul>
+            </div>
+            2. 第二种写法，少一层div包裹，可以省略一些css
+             */
+            
             <Fragment>
                 <div>
                     <label htmlFor='js'>需求：</label>
                     {/* bind()是es6中的语法,用来绑定this */}
-                    <input id='js' className='input' value={this.state.inputValue} onChange={this.inputChange.bind(this)}/> 
+                    <input 
+                        id='js' 
+                        className='input' 
+                        value={this.state.inputValue} 
+                        onChange={this.inputChange.bind(this)}
+                        // 通过ref属性进行绑定
+                        ref = {(input)=>{this.input=input}}
+                    /> 
                     <button onClick={this.addList.bind(this)}>添加需求</button>
                 </div>
-                <ul>
+                <ul ref={(ul)=>{this.ul=ul}}>
                     {
                         this.state.list.map((item,index)=>{
                         return (
-                            <li 
-                                key={index+item}
-                                onClick={this.deleteItem.bind(this,index)}
-                                // html解析
-                                dangerouslySetInnerHTML={{__html:item}}
-                            >
-                            </li>
+                                // 父组件向子组件按属性传值
+                                <FullstackItem
+                                    key={index+item}
+                                    content={item}
+                                    index={index}
+                                    deleteItem={this.deleteItem.bind(this)}
+                                 />
                         )
                         })
                     }
@@ -48,9 +103,11 @@ class Fullstack extends Component{
         )
     }
 
-    inputChange(e){
+    inputChange(){
         this.setState({
-            inputValue: e.target.value
+            // this.state.input.value = e.target.value
+            // 下面的写法语义更强
+            inputValue: this.input.value
         })
     }
 
@@ -59,6 +116,8 @@ class Fullstack extends Component{
         this.setState({
             list:[...this.state.list,this.state.inputValue],
             inputValue:''
+        },()=>{
+            console.log(this.ul.querySelectorAll('li').length)
         })
     }
 
---
+++ demo01/src/FullstackItem.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+
+class FullstackItem extends Component {
+
+    constructor(props){
+        super(props)
+        this.handleClick=this.handleClick.bind(this)
+    }
+
+    /**
+     * 组件第一次存在与dom中，函数不会被执行
+     * 如果已经存在于dom中，函数才会被执行
+     */
+    componentWillReceiveProps(){
+        console.log('child-componentWillReceiveProps')
+    }
+
+    componentWillUnmount(){
+        console.log('child-componentWillUnmount')
+    }
+
+    render() { 
+        return ( 
+            <li onClick={this.handleClick}>
+                {this.props.pgname}负责-{this.props.content}
+            </li>
+         );
+    }
+
+    handleClick(){
+        this.props.deleteItem(this.props.index)
+    }
+}
+
+FullstackItem.propTypes={
+    pgname:PropTypes.string.isRequired,
+    content:PropTypes.string,
+    index:PropTypes.number,
+    deleteItem:PropTypes.func
+}
+
+FullstackItem.defaultProps={
+    pgname:'linus'
+}
+
+export default FullstackItem
